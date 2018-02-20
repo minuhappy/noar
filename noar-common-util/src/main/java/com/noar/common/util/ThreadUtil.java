d8bb1f7a57bbfd42cb963ac5fe2cd005dd870c98
Minwoo-Kim <bluesky@hatiolab.com>
2018-02-20 16:34:43 +0900

    .

---
+++ noar-common-util/src/main/java/com/noar/common/util/ThreadUtil.java
@@ -0,0 +1,64 @@
+package com.noar.common.util;
+
+public class ThreadUtil {
+
+	/**
+	 * 동기 방식의 스레드 실행.
+	 * 
+	 * @param runnable
+	 */
+	public static String doSynch(Runnable runnable) {
+		return new SimpleThread(runnable).start();
+	}
+
+	/**
+	 * 비동기 방식의 스레드 실행.
+	 * 
+	 * @param runnable
+	 */
+	public static void doAsynch(Runnable runnable) {
+		new AsynchThread(runnable).start();
+	}
+}
+
+/**
+ * 동기 방식의 스레드 클래스.
+ * 
+ * @author minu
+ */
+class SimpleThread {
+	Runnable runnable;
+
+	public SimpleThread(Runnable runnable) {
+		this.runnable = runnable;
+	}
+
+	public String start() {
+		double start = System.currentTimeMillis();
+		runnable.run();
+		return ValueUtil.toString(System.currentTimeMillis() - start);
+	}
+}
+
+/**
+ * 비동기 방식의 스레드 클레스.
+ * 
+ * @author minu
+ */
+class AsynchThread implements Runnable {
+	Runnable runnable;
+
+	public AsynchThread(Runnable runnable) {
+		this.runnable = runnable;
+	}
+
+	public void start() {
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void run() {
+		runnable.run();
+	}
+}
